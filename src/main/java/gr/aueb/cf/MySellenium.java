1ff4f86daafa0570e009ec1969a1d909fb0e8757
apstav <apostolis.stav@gmail.com>
2023-03-30 18:54:50 +0300

    initial commit

---
+++ src/main/java/gr/aueb/cf/MySellenium.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MySellenium {
+    public static void main(String[] args) {
+
+        
+        WebDriver driver = new ChromeDriver();
+    }
+}
